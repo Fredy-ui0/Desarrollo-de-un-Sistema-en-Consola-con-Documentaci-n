b5e00d3c8ac1eb9f47ce4238f4cbedebf4030c87
rudycardona4056-ctrl <rudycardona4056@gmail.com>
2026-03-19 18:51:26 -0600

    v2
    mostramos los libros que tenemos almacenados

---
+++ Sistema de Gestión de Biblioteca/version2.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("Titulo" + tlib);      /*nostramos el titulo ingresado*/
+Console.WriteLine("Autor" + aut);        /*mostramos el autor ingresado*/
+Console.WriteLine("Categoria" + cat);    /*mostramos la categoria ingresada*/
+    
+
+
+
+
