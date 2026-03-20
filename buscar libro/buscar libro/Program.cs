978063ed5020e7ee645fbbcff9124f28590a2005
sergioescalanteh <136497506+sergioescalanteh@users.noreply.github.com>
2026-03-19 19:13:04 -0600

    Sistema de Gestión de biblioteca

---
+++ buscar libro/buscar libro/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] libros = { "Cien años de soledad", "El principito", "Don Quijote", "La sombra del viento" };
+
+        Console.Write("Ingrese el título del libro que desea buscar: ");
+        string tituloBuscado = Console.ReadLine();
+
+bool encontrado = false;
+
+foreach (string libro in libros)
+{
+// evitar problemas de mayúsculas/minúsculas o espacios
+    if (libro.ToLower().Trim() == tituloBuscado.ToLower().Trim())
+    {
+        encontrado = true;
+        break;
+    }
+}
+if (encontrado)
+{
+    Console.WriteLine("El libro existe en el sistema.");
+}
+else
+{
+    Console.WriteLine("El libro no está registrado.");
+}
