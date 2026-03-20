a989efb3cacb866013f96d84ccc54ff480f6f5cd
Degly Mateo <deglyMX@outlook.com>
2026-03-19 21:04:42 -0600

    Registro de Prestamo

---
+++ 4. Registrar Prestamo/4. Registrar Prestamo/Program.cs
@@ -0,0 +1,26 @@
+﻿// Registro de Prestamo
+string tlib = "Clean Code";
+bool estaPrestado = false;
+
+Console.WriteLine("--- Módulo de Préstamos ---");
+Console.Write("Usuario: ");
+string usuario = Console.ReadLine();
+Console.Write("Libro a prestar: ");
+string tituloPrestamo = Console.ReadLine();
+
+if (tituloPrestamo.ToUpper() == tlib.ToUpper())
+{
+    if (!estaPrestado)
+    {
+        estaPrestado = true;
+        Console.WriteLine("Préstamo exitoso a: " + usuario);
+    }
+    else
+    {
+        Console.WriteLine("El libro ya está prestado.");
+    }
+}
+else
+{
+    Console.WriteLine("Libro no encontrado.");
+}
